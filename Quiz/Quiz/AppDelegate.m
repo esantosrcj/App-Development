3e84a69036f472b2585e1300f18624f35298213c
Eduardo Santos <esantosrcj@gmail.com>
2016-09-01 15:08:48 -0500

    Add updated files for the Quiz app

--- Quiz/Quiz/AppDelegate.m
+++ Quiz/Quiz/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BNRViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,16 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // To get the BNRViewController on screen, you need to make it the root view controller of this window.
+    BNRViewController *quizVC = [[BNRViewController alloc] init];
+    self.window.rootViewController = quizVC;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 

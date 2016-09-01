4d2d2a8c032671464b157163ce852b3d2846b809
Eduardo Santos <esantosrcj@gmail.com>
2016-09-01 13:02:09 -0500

    Quiz application

---
+++ Quiz/BNRViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRViewController.h
+//  Quiz
+//
+//  Created by Eduardo on 9/1/16.
+//  Copyright © 2016 Red Rojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRViewController : UIViewController
+
+@end
---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Eduardo on 9/1/16.
+//  Copyright © 2016 Red Rojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

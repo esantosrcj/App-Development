bfaa147dd26d5f7d3c297c4bc76d4b30e4e29f77
Eduardo Santos <esantosrcj@gmail.com>
2016-09-02 12:19:05 -0500

    Add updated version of RandomItem

--- RandomItems/RandomItems/BNRItem.h
+++ RandomItems/RandomItems/BNRItem.h
@@ -10,4 +10,36 @@
 
 @interface BNRItem : NSObject
 
+{
+    // Instance variables
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
+// + symbol signifies a CLASS method
++ (instancetype)randomItem;
+
+// Designated initializer for BNRItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+// Getters and Setters
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+
+
 @end
--- RandomItems/RandomItems/main.m
+++ RandomItems/RandomItems/main.m
@@ -7,11 +7,26 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        // Create a mutable arrya object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        for (int i = 0; i < 10; i++) {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
+        }
+        
+        for (BNRItem *item in items) {
+            NSLog(@"%@", item);
+        }
+        
+        // Destroy the mutable array object
+        items = nil;
+        
     }
     return 0;
 }

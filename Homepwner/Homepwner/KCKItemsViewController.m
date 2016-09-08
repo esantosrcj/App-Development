f5eb9df2b6e8c1505bbac6148d39a6f3241e9ad2
Eduardo Santos <esantosrcj@gmail.com>
2016-09-08 18:41:09 -0500

    Updated UITableViewCell to reuse cell

--- Homepwner/Homepwner/KCKItemsViewController.m
+++ Homepwner/Homepwner/KCKItemsViewController.m
@@ -45,9 +45,9 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Create an instance of UITableViewCell, with default appearance
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-                                                   reuseIdentifier:@"UITableViewCell"];
+    // Get a new or recycled cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                                            forIndexPath:indexPath];
     
     // Set the text on the cell with the description of the item that is at the nth index of
     // items, where n = row this cell will appear in on the tableView
@@ -57,6 +57,14 @@
     cell.textLabel.text = [item description];
     
     return cell;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
 }
 
 @end

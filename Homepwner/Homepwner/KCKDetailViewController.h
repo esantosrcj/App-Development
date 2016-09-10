3ad7bf4059be3ee62c209d1c48cefac8b49e6c70
Eduardo Santos <esantosrcj@gmail.com>
2016-09-10 17:36:36 -0500

    Add KCKDetailViewController to Homepwner

---
+++ Homepwner/Homepwner/KCKDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  KCKDetailViewController.h
+//  Homepwner
+//
+//  Created by Eduardo on 9/10/16.
+//  Copyright © 2016 Red Rojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KCKDetailViewController : UIViewController
+
+@end

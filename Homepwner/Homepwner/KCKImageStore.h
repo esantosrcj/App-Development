1ebf50c5650a967522888be857568306d4acf63d
Eduardo Santos <esantosrcj@gmail.com>
2016-09-12 11:33:42 -0500

    Add image store to hold pictures the user takes

---
+++ Homepwner/Homepwner/KCKImageStore.h
@@ -0,0 +1,13 @@
+//
+//  KCKImageStore.h
+//  Homepwner
+//
+//  Created by Eduardo on 9/12/16.
+//  Copyright © 2016 Red Rojo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KCKImageStore : NSObject
+
+@end
---
+++ Homepwner/Homepwner/KCKImageStore.m
@@ -0,0 +1,13 @@
+//
+//  KCKImageStore.m
+//  Homepwner
+//
+//  Created by Eduardo on 9/12/16.
+//  Copyright © 2016 Red Rojo. All rights reserved.
+//
+
+#import "KCKImageStore.h"
+
+@implementation KCKImageStore
+
+@end
